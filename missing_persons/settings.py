03f3e76bf9be3ba138be42fb7d429fc6d99f9d3c
jaanu <subhanmd606@gmail.com>
2026-03-14 14:52:24 +0530

    Secure SECRET_KEY using environment variable

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-myj_1x-&*momc0)4h+6duy$pb16ri&$&ljpzsot!$qfv5@!mg6'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-myj_1x-&*momc0)4h+6duy$pb16ri&$&ljpzsot!$qfv5@!mg6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
